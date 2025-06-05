2679b6293371ecf6b77054ef550d2352d0e18b36
Shubham91999 <skulka86@asu.edu>
2025-06-04 21:50:23 -0700

    Started with POST request

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
-from fastapi import FastAPI
+from fastapi import Body, FastAPI
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"message": "Welcome to my API"}
+
+@app.get("/posts")
+def get_posts():
+    return {"Post1": "Data for Post1", "Post2": "Data for Post2"}
+
+@app.post("/createpost")
+def create_post(payload: dict = Body(...)):
+    print(payload)
+    return {"NewPost": f"Title: {payload['title']} Content: {payload['content']}"} 
+
