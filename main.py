d86b50fc955160e4e10ae3d598047627954d3442
Shubham91999 <skulka86@asu.edu>
2025-06-07 21:23:59 -0700

    Started with CRUD Operations

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 from fastapi import Body, FastAPI
+from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
+
+# Defined Pydantic model to validate the input schema from frontend
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
 
 @app.get("/")
 async def root():
@@ -10,8 +19,15 @@ async def root():
 def get_posts():
     return {"Post1": "Data for Post1", "Post2": "Data for Post2"}
 
+# @app.post("/createpost")
+# def create_post(payload: dict = Body(...)):
+#     print(payload)
+#     return {"NewPost": f"Title: {payload['title']} Content: {payload['content']}"} 
+
 @app.post("/createpost")
-def create_post(payload: dict = Body(...)):
-    print(payload)
-    return {"NewPost": f"Title: {payload['title']} Content: {payload['content']}"} 
+def create_post(post: Post):
+    print(post)  # printing data in BaseModel format
+    print(post.model_dump())  # dict() method is deprecated, so using model_dump() -> printing result in form of dictionary
+    return {"data": post}
+
 
