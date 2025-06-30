3bac6ffb27442cd6ccfb1947db8a9bde4a72c018
Shubham91999 <skulka86@asu.edu>
2025-06-29 17:25:58 -0700

    Started with User routers

--- app/models.py
+++ app/models.py
@@ -11,5 +11,13 @@ class Post(Base):
     published = Column(Boolean, default=True)
     created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
 
+# Class for creating User table
+class User(Base):
+    __tablename__ = "users"
+    email = Column(String, nullable=False, unique=True)
+    password = Column(String, nullable=False)
+    id = Column(Integer, primary_key=True, nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
+
 
     
