d8aac09d1667bac2f1f34d3de409f9c23fc417d9
Shubham91999 <skulka86@asu.edu>
2025-07-02 20:22:25 -0700

    Started with user routes

--- app/main.py
+++ app/main.py
@@ -168,12 +168,20 @@ def create_user(user: UserCreate, db: Session = Depends(get_db)):
     hashed_password = utils.hash(user.password)
     user.password = hashed_password
 
-
     new_user = models.User(**user.dict())  # Unpacking the user object from request into dictionary
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
     return new_user
+
+@app.get("/users/{id}")
+def get_user(id: int, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == id).first() # type: ignore
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"User with id: {id} does not exist")
+    return user
+
+
 
 
 
