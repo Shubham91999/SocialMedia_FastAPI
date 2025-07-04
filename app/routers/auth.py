4a60af7379c67309e2acff001f1963840cf38738
Shubham91999 <skulka86@asu.edu>
2025-07-03 19:35:03 -0700

    JWT Configured Successfully

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends, status, HTTPException, Response
 from ..database import get_db
 from sqlalchemy.orm import Session
-from .. import schemas, models, utils
+from .. import schemas, models, utils, oauth2
 
 
 
@@ -17,5 +17,5 @@ def login(user_creds: schemas.UserLogin, db: Session = Depends(get_db)):
     if not utils.verify(user_creds.password, user.password):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Invalid Credentials")
     
-
-    return {"token": "Login Successful"}
+    access_token = oauth2.create_access_token(data={"user_id": user.id})
+    return {"token": access_token, "token_type": "bearer"}
